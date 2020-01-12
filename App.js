5594ced72fbf3248735469d929ba5c58f342eb9f
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-01-12 21:36:12 +0000

    Fonts now loading into app correctly.

--- App.js
+++ App.js
@@ -4,11 +4,25 @@ import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { setCustomText } from 'react-native-global-props';
 
 import AppNavigator from './navigation/AppNavigator';
 
+function cacheFonts(fonts) {
+  console.log('inside cache')
+  return fonts.map(font => Font.loadAsync(font));
+}
+
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+// const customTextProps = {
+//   style: {
+//     fontFamily: 'Cute Fonts'
+//   }
+// };
+
+// setCustomText(customTextProps);
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
@@ -29,19 +43,28 @@ export default function App(props) {
 }
 
 async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'Work Sans': require('./assets/fonts/WorkSans-Regular.ttf')
-    }),
-  ]);
+  // await Promise.all([
+  //   Asset.loadAsync([
+  //     require('./assets/images/robot-dev.png'),
+  //     require('./assets/images/robot-prod.png'),
+  //   ]),
+  //   Font.loadAsync({
+  //     // This is the font that we are using for our tab bar
+  //     ...Ionicons.font,
+  //     // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+  //     // remove this if you are not using it in your app
+  //     'Work Sans': require('./assets/fonts/WorkSans-Regular.ttf')
+  //   })
+  // ]);
+
+  const fonts = [
+    {'Work Sans': require('./assets/fonts/WorkSans-Regular.ttf')},
+    {'Nunito Sans': require('./assets/fonts/NunitoSans-Regular.ttf')},
+    {'Cute Font': require('./assets/fonts/CuteFont-Regular.ttf')}
+  ]
+
+  const fontAssets = cacheFonts(fonts);
+  await Promise.all([...fontAssets]);
 }
 
 function handleLoadingError(error) {
@@ -58,5 +81,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    fontFamily: 'Cute Font'
   },
 });
