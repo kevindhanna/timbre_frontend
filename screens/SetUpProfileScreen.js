4e389c7b6a5b5ee7e6f9e901bcea85ddce735ed4
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-04-04 20:59:58 +0100

    fixes scrolling issue on setting up profile.

--- screens/SetUpProfileScreen.js
+++ screens/SetUpProfileScreen.js
@@ -3,7 +3,8 @@ import React, { Component, createRef } from 'react';
 import {
   StyleSheet,
   View,
-  ScrollView
+  ScrollView,
+  SafeAreaView
 } from 'react-native';
 import InstrumentMultiSelect from '../components/InstrumentMultiSelect';
 import InstrumentRating from '../components/InstrumentRating';
@@ -44,7 +45,8 @@ class SetUpProfileScreen extends Component {
   }
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <ScrollView style={styles.container}
+      >
         <View style={styles.formContainer}>
           {this.formType()}
         </View>
@@ -55,11 +57,17 @@ class SetUpProfileScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: 'red',
+    paddingBottom: 10,
   },
   formContainer: {
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
+    paddingBottom: 100,
+    borderWidth: 2,
+    borderColor: 'green',
+    height: 1000
   },
 })
 
